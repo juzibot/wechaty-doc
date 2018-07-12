ac907c2aebf8a2759e8c0f9feb13c50759eed631
Jiarui LI <ruiruibupt@126.com>
2018-07-12 16:38:37 +0800

    cat local file

--- gen_api_from_remote.sh
+++ gen_api_from_remote.sh
@@ -2,4 +2,5 @@
 
 # https://raw.githubusercontent.com/lijiarui/wechaty/add-doc/docs/index.md
 
-curl https://raw.githubusercontent.com/lijiarui/wechaty/add-doc/docs/index.md | python3 parse_doc.py
+# curl https://raw.githubusercontent.com/lijiarui/wechaty/add-doc/docs/index.md | python3 parse_doc.py
+cat /Users/jiaruili/git/rui/wechaty/docs/index.md | python3 parse_doc.py
